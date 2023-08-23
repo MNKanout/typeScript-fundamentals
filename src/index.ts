6719f9813bbf86949377c4aae72d6dbed6ddcd78
Nour Kanout <nkanout@identecsolutions.com>
2023-08-23 14:34:06 +0200

    Added arrays

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+// Basic built-in types
 // With type annotation
 let sales: number = 123_456_789;
 let course: string = 'TypeScript';
@@ -9,3 +10,14 @@ let level: any;
  let course_1 = 'TypeScript';
  let is_published_1 = true;
  let level_1; // "any" type
+
+//  Arrays
+//  With type annotation
+let numbers: number[] = [1,2,3] // Array of numbers
+let strings: string[] = ['1','2','3'] // Array of strings
+let any_array: any[] = [1,"2", true] // Array of different types of elements
+
+//  Without type annotation
+let numbers_1 = [1,2,3] // Array of numbers
+let strings_1 = ['1','2','3'] // Array of strings
+let any_array_1 = [1,"2", true] // Array of different types of elements
