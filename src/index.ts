5b59fed97171a190d2f2ef798101934a368eeb25
Nour Kanout <nkanout@identecsolutions.com>
2023-08-23 14:24:49 +0200

    Added basic types, number, string, boolean, any

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,11 @@
-let age: number = 20;
-console.log(age);
-if (age < 50) {
-    age += 10;
-}
-console.log(age);
+// With type annotation
+let sales: number = 123_456_789;
+let course: string = 'TypeScript';
+let is_published: boolean = true;
+let level: any; 
+
+// Without type annotation
+ let sales_1 = 123_456_789;
+ let course_1 = 'TypeScript';
+ let is_published_1 = true;
+ let level_1; // "any" type
